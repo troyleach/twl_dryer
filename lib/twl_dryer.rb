253dc627fba5068ad1f659c0a7dfd15b8110f8b7
Troy Leach <troyleach29@gmail.com>
2015-03-19 19:02:05 -0500

    fixed link bug

--- lib/twl_dryer.rb
+++ lib/twl_dryer.rb
@@ -3,7 +3,7 @@ require "twl_dryer/version"
 module TwlDryer
   class Dryer
     def dry
-      'say oooooooooooooooooooo ooooooooooooooooo ooooooooooooo ooooooooo'
+      `say oooooooooooooooooooo ooooooooooooooooo ooooooooooooo ooooooooo`
     end
   end
 end
